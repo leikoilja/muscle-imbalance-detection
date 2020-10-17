6086e9aa5d06535903475953509fe4ac0db3832b
Ilja Leiko <leikoilja@gmail.com>
2020-10-17 19:18:04 +0200

    Disable the app if not connected to the internet

--- mobile_app/mid/src/App.tsx
+++ mobile_app/mid/src/App.tsx
@@ -1,22 +1,50 @@
-import React from 'react';
+import React from "react";
 
-import Toast from 'react-native-toast-message';
-import * as eva from '@eva-design/eva';
-import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
-import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import { SafeAreaView } from 'react-native';
-import { connect } from 'react-redux';
-import RootNavigator from './navigation/RootNavigator';
+import Toast from "react-native-toast-message";
+import * as eva from "@eva-design/eva";
+import { ApplicationProvider, IconRegistry, Text } from "@ui-kitten/components";
+import { EvaIconsPack } from "@ui-kitten/eva-icons";
+import { StyleSheet, SafeAreaView, View } from "react-native";
+import { connect } from "react-redux";
+import NetInfo from "@react-native-community/netinfo";
+import RootNavigator from "./navigation/RootNavigator";
 
 class App extends React.Component {
+  state = {
+    isConnected: false,
+  };
+
+  componentDidMount() {
+    this._subscription = NetInfo.addEventListener(this._handleIsConnected);
+  }
+
+  componentWillUnmount() {
+    this._subscription && this._subscription();
+  }
+
+  _handleIsConnected = (connectionInfo: NetInfoState) => {
+    this.setState(({ isConnected }) => ({
+      isConnected: connectionInfo.isConnected,
+    }));
+  };
+
   render() {
     const { theme } = this.props;
     return (
       <ApplicationProvider {...eva} theme={eva[theme]}>
         <Toast ref={(ref) => Toast.setRef(ref)} />
         <IconRegistry icons={EvaIconsPack} />
-        <SafeAreaView style={{ flex: 1 }}>
-          <RootNavigator />
+        <SafeAreaView style={styles.container}>
+          {this.state.isConnected ? (
+            <RootNavigator />
+          ) : (
+            <View style={styles.notConnectedContainer}>
+              <Text style={styles.notConnectedText} status="danger">
+                We are sorry, but seems like you are not connected to the
+                internet. The app will automatically reload once you are online
+              </Text>
+            </View>
+          )}
         </SafeAreaView>
       </ApplicationProvider>
     );
@@ -30,3 +58,17 @@ const mapStateToProps = (state) => ({
 const mapDispatchToProps = {};
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+  notConnectedContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  notConnectedText: {
+    margin: 20,
+    alignSelf: "center",
+    textAlign: "center",
+  },
+});
